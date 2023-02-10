d939e8377dbe6445b23b9628283f465fc585ad6f
Saad28may <saadshafiq2019@gmail.com>
2023-02-11 02:58:55 +0500

    How to send value with POST method

--- form1.php
+++ form1.php
@@ -1,16 +1,8 @@
 
-<form method="POST">
+<form action="form2.php" method="POST">
 <h1>Form 1</h1>
 Color : <br><input type="radio" name="color" value="red">Red
 <input type="radio" name="color" value="green">Green
 <button type= "submit" name="submit">Submit </button>
 </form>
 
-<?php
-if(isset($_POST["submit"])){
-session_start();
-$_SESSION['form1'] = $_POST["color"];
-header('Location: form2.php');
-exit;
-}
-?>
--- form2.php
+++ form2.php
@@ -1,23 +1,25 @@
+<!-- how to set value of previous page into html -->
+
 <form method="POST">
 <h1>Form 2</h1>
-Color : <br><input type="radio" name="color" value="red">Red
-<input type="radio" name="color" value="green">Green
+Color : <br><input type="radio" name="color2" value="red">Red
+<input type="radio" name="color2" value="green">Green
+<input type="hidden" name="form1val" value="<?php echo $_POST["color"] ?>">
 <button type= "submit" name="submit2">Submit </button>
 </form>
 
+
 <?php
-session_start();
 
 if(isset($_POST["submit2"])){
 
-$_SESSION['form2'] = $_POST["color"];
+$form2 = $_POST["color2"];
 
-
-if($_SESSION['form1']== "green" && $_SESSION['form2']=="green"){
+if( $_POST["form1val"] == "green" && $form2=="green"){
     header('Location: green.php');
     exit;
 }
-elseif($_SESSION['form1']== "red" && $_SESSION['form2']=="red"){
+elseif( $_POST["form1val"] == "red" && $form2=="red"){
     header('Location: red.php');
     exit;
 }
