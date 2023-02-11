b086636c5a4a27657f587d8305704a8875775580
Saad28may <saadshafiq2019@gmail.com>
2023-02-11 22:31:36 +0500

    task1

---
+++ Task1-10/task1.php
@@ -0,0 +1,12 @@
+<?php
+
+// Get the PHP version
+echo 'PHP Version: ' . phpversion() . "\n";
+
+// Get the PHP configuration information
+$php_config = phpinfo();
+
+// Display the PHP configuration information
+echo $php_config;
+
+?>
